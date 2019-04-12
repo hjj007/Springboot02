dd0821e3f627ac53af3fc6ca739b565ef5239cd7
hjj007 <49516835+hjj007@users.noreply.github.com>
2019-04-12 10:00:54 +0800

    Update FastJsonConf.java

--- src/main/java/com/baizhi/conf/FastJsonConf.java
+++ src/main/java/com/baizhi/conf/FastJsonConf.java
@@ -18,6 +18,57 @@ public class FastJsonConf {
         //2. 添加fastjson的配置信息，比如:是否需要格式化返回的json的数据
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
         //3. 在converter中添加配置信息
         fasHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
